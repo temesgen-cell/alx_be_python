404829811932f33084e7658086e8cb4edfafd5d9
temesgen-cell <temesgensids@gmail.com>
2025-09-22 19:20:51 +0300

    patern resolved

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,8 @@
-size = int(input("Enter the size of the pattern: ")
+siz = (input("Enter the size of the pattern: ")
+size=int(siz)
 row = 0
 while row < size:
     for col in range(size):
-        print("*", end="")  # Print asterisk without newline
-    print()  # Move to the next row
+        print("*", end="")  
+    print()  
     row += 1
