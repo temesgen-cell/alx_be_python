b580f5162e510c3f4dd3838c8de7cd49428a0b15
temesgen-cell <temesgensids@gmail.com>
2025-09-28 16:21:18 +0300

    final modifiacation commited

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,16 @@
-def perform_operation(num1,num2,operation):
-    if z=="add":
-        return x+y
-    elif z=="subtract":
-        return x-y
-    elif z=="multiply":
-        return x*y
-    elif z=="divide":
-        if y==0:
-            return print("division by zero is not defined")
-        else:
-            return x/y
+
+def perform_operation(num1: float, num2: float, operation: str):
+
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
+
