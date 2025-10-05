860eaa9fd23f5465a833e42d18d977516ffdda5c
temesgen-cell <temesgensids@gmail.com>
2025-10-06 02:11:58 +0300

    the bank system is added

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self,account_balance=0):
+        self.account_balance=account_balance
+
+    def deposit(self,amount):
+        self.account_balance+=amount
+
+    def withdraw(amount):
+        if self.account_balance < amount:
+            return False
+        else:
+
+            return True
+
+    def display_balance():
+        print(f"current balance is ${self.account_balance}")
