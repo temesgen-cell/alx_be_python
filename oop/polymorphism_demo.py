f6b07ed957c854b2aced1d96616b9b8a92bbb449
temesgen-cell <temesgensids@gmail.com>
2025-10-12 17:56:23 +0300

    poly demo

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,16 @@
+import math
+class Shape:
+    def area(self):
+        raise NotImplementedError("this shall be over rided in order to make it fully functional")
+
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length=length
+        self.width=width
+    def area(self):
+        return self.length*self.width
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius=radius
+    def area(self):
+        return math.pi * self.radius * self.radius
